9cdad95af079db3e31cddaea007aabe26e98817f
Rodrigo <rodrigo300184@gmail.com>
2023-11-08 17:27:28 +0100

    user service and controller to SQL

--- dist/services/usersService.js
+++ dist/services/usersService.js
@@ -10,43 +10,47 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.usersService = void 0;
-const usersModel_1 = require("../models/usersModel");
+const api_connection_1 = require("../utils/api_connection");
 function fetchAll() {
     return __awaiter(this, void 0, void 0, function* () {
-        const getAllUsers = yield usersModel_1.User.find();
-        if (!getAllUsers)
-            throw new Error('Error obtaining all users');
+        const getAllUsers = yield (0, api_connection_1.selectQuery)('SELECT * FROM user;');
         return getAllUsers;
     });
 }
 function fetchOne(id) {
     return __awaiter(this, void 0, void 0, function* () {
-        const user = yield usersModel_1.User.findById(id);
-        if (!user)
+        const user = yield (0, api_connection_1.selectQuery)(`SELECT * FROM user WHERE id = ?`, [id]);
+        if (!user.length)
             throw new Error("Error obtaining the user or the user doesn't exist");
-        return user;
+        return user[0];
     });
 }
 function createOne(user) {
     return __awaiter(this, void 0, void 0, function* () {
-        const newUser = yield usersModel_1.User.create(user);
-        if (!newUser)
+        const query = `INSERT INTO user (full_name, email, photo, start_date, description, phone_number, status) VALUES (?, ?, ?, ?, ?, ?, ?);`;
+        const data = [user.full_name, user.email, user.photo, user.start_date, user.description, user.phone_number, user.status];
+        const newUser = yield (0, api_connection_1.selectQuery)(query, data);
+        if (newUser.affectedRows === 0)
             throw new Error("The user couldn't be created");
-        return newUser;
+        const createdUser = yield fetchOne(newUser.insertId);
+        return createdUser;
     });
 }
 function editOne(id, update) {
     return __awaiter(this, void 0, void 0, function* () {
-        const updatedUser = yield usersModel_1.User.findByIdAndUpdate(id, update);
-        if (!updatedUser)
+        const query = `UPDATE user SET full_name = ?, email = ?, photo = ?, start_date = ?, description = ?, phone_number = ?, status = ? WHERE id = ?;`;
+        const data = [update.full_name, update.email, update.photo, update.start_date, update.description, update.phone_number, update.status, id];
+        const userUpdate = yield (0, api_connection_1.selectQuery)(query, data);
+        if (!userUpdate)
             throw new Error("The user doesn't exist or couldn't be updated");
+        const updatedUser = yield fetchOne(id);
         return updatedUser;
     });
 }
 function deleteOne(id) {
     return __awaiter(this, void 0, void 0, function* () {
-        const deletedUser = yield usersModel_1.User.findByIdAndDelete(id);
-        if (!deletedUser)
+        const deletedUser = yield (0, api_connection_1.selectQuery)(`DELETE FROM user WHERE id = ?`, [id]);
+        if (deletedUser.affectedRows === 0)
             throw new Error("The user doesn't exist or couldn't be deleted");
         return;
     });
--- services/usersService.ts
+++ services/usersService.ts
@@ -1,33 +1,39 @@
 import { User } from '../models/usersModel';
 import { UsersInterface } from '../interfaces/usersInterface';
+import { selectQuery } from '../utils/api_connection';
 
 async function fetchAll() {
-  const getAllUsers = await User.find();
-  if (!getAllUsers) throw new Error('Error obtaining all users');
+  const getAllUsers = await selectQuery('SELECT * FROM user;');
   return getAllUsers;
 }
 
 async function fetchOne(id: string) {
-  const user = await User.findById(id);
-  if (!user) throw new Error("Error obtaining the user or the user doesn't exist");
-  return user;
+  const user = await selectQuery(`SELECT * FROM user WHERE id = ?`, [id]);
+  if (!user.length) throw new Error("Error obtaining the user or the user doesn't exist");
+  return user[0];
 }
 
 async function createOne(user: UsersInterface) {
-  const newUser = await User.create(user);
-  if(!newUser) throw new Error("The user couldn't be created")
-  return newUser;
+  const query = `INSERT INTO user (full_name, email, photo, start_date, description, phone_number, status) VALUES (?, ?, ?, ?, ?, ?, ?);`;
+  const data = [user.full_name, user.email, user.photo, user.start_date, user.description, user.phone_number, user.status];
+  const newUser = await selectQuery(query, data);
+  if(newUser.affectedRows === 0) throw new Error("The user couldn't be created")
+  const createdUser = await fetchOne(newUser.insertId);
+  return createdUser;
 }
 
 async function editOne(id: string, update: Partial<UsersInterface>) {
-  const updatedUser = await User.findByIdAndUpdate(id,update);
-  if (!updatedUser) throw new Error("The user doesn't exist or couldn't be updated");
+  const query = `UPDATE user SET full_name = ?, email = ?, photo = ?, start_date = ?, description = ?, phone_number = ?, status = ? WHERE id = ?;`;
+  const data = [update.full_name,update.email,update.photo,update.start_date,update.description,update.phone_number,update.status,id];
+  const userUpdate = await selectQuery(query, data);
+  if (!userUpdate) throw new Error("The user doesn't exist or couldn't be updated");
+  const updatedUser = await fetchOne(id);
   return updatedUser;
 }
 
 async function deleteOne(id: string) {
-  const deletedUser = await User.findByIdAndDelete(id);
-  if (!deletedUser) throw new Error("The user doesn't exist or couldn't be deleted");
+  const deletedUser = await selectQuery(`DELETE FROM user WHERE id = ?`, [id]);
+  if (deletedUser.affectedRows === 0) throw new Error("The user doesn't exist or couldn't be deleted");
   return
 }
 
